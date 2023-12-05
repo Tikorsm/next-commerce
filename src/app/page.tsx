1f560cb9b68cdfe54c087a7eb90e66180d333f67
Tikorsm <isaiassaomiguel8@gmail.com>
2023-12-05 16:16:25 -0300

    listagem de pruduto 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+      
       <h1> Next</h1>
+      <h1> Next</h1>
+      </div>
 
     </div>
   )
