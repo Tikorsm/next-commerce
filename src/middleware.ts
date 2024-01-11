d1d59316ab4e8010b7dc5cc254b729c4db4c125d
Tikorsm <isaiassaomiguel8@gmail.com>
2024-01-11 07:54:05 -0300

    Stripe webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: ['/', '/product(.*)', '/sign-in', '/sign-up', '/api/webhooks/user'],
+    publicRoutes: ['/', '/product(.*)', '/sign-in', '/sign-up', '/api/webhooks/user', '/api/webhook/stripe'],
 });
  
 export const config = {
